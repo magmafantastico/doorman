39552461cf3a4bc71d8a600730af3ea19dccb4f3
dudusbarros <joseeduardobarros@gmail.com>
2015-10-03 10:42:15 -0300

    Add websockets feature Add Sensor test Add Led Test

--- js/led.js
+++ js/led.js
@@ -3,20 +3,25 @@ var board = new five.Board();
 
 board.on("ready", function() {
 
-	var led2 = new five.Led(3);
+	//var led2 = new five.Led(2);
+	//
+	//var led2ligado = false;
+	//
+	//setInterval(function() {
+	//
+	//	if(led2ligado) {
+	//		led2.off();
+	//	} else {
+	//		led2.on();
+	//	}
+	//	led2ligado = !led2ligado;
+	//	console.log(led2ligado);
+	//
+	//}, 2000);
 
-	var led2ligado = false;
-
-	setInterval(function() {
-
-		if(led2ligado) {
-			led2.off();
-		} else {
-			led2.on();
-		}
-		led2ligado = !led2ligado;
-		console.log(led2ligado);
-
-	}, 2000);
+	for (var i = 0; i < 15; i++) {
+		var led = new five.Led(i);
+		led.on();
+	}
 
 });
